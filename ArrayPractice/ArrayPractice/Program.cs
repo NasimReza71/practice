9af716cb395702561387ee3ae53431defa58dcb7
Nasim Reza Hridoy <nasim.reza201@gmail.com>
2023-06-02 18:24:37 +0600

    project added

---
+++ ArrayPractice/ArrayPractice/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+Console.WriteLine("Write Your Name");
+string x = Console.ReadLine();
+
+string[] inputparts = x.Split(',',',');
+Console.WriteLine($"Name : {inputparts[0]}, Age: {inputparts[1]}");
+
+
+
+
+
+
